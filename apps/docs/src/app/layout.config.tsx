8f18f2121a48f1a4aaa1e8295a89c5a6856d1e37
ping-maxwell <maxwell.multinite@gmail.com>
2025-02-27 05:15:46 +1000

    WIP: fixing docs navbar

--- apps/docs/src/app/layout.config.tsx
+++ apps/docs/src/app/layout.config.tsx
@@ -1,5 +1,5 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import Image from 'next/image';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
 
 /**
  * Shared layout configurations
@@ -12,16 +12,32 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <Image src="/logo/500x500.png" alt="Better Auth Kit" width={32} height={32} />
+        <Image
+          src="/logo/500x500.png"
+          alt="Better Auth Kit"
+          width={32}
+          height={32}
+        />
         Better Auth Kit
       </>
+    ),
+    component: (
+      <div className="flex flex-col items-center justify-center h-10 w-screen">
+        <Image
+          src="/logo/500x500.png"
+          alt="Better Auth Kit"
+          width={32}
+          height={32}
+        />
+        Better Auth Kit
+      </div>
     ),
   },
   links: [
     {
-      text: 'Documentation',
-      url: '/docs',
-      active: 'nested-url',
+      text: "Documentation",
+      url: "/docs",
+      active: "nested-url",
     },
   ],
 };
