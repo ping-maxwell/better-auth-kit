c89c38f313be49fc812e3af01c28a9bc9ac46670
ping-maxwell <maxwell.multinite@gmail.com>
2025-02-27 04:21:15 +1000

    fix: github metadata title

--- apps/docs/src/app/github/page.tsx
+++ apps/docs/src/app/github/page.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from "next";
 import Redirect from "./redirect";
 
 export const metadata: Metadata = {
-  title: "Better Auth Kit Github",
+  title: "Better-Auth-Kit Github",
   description: "The Better-Auth-Kit GitHub repository.",
   openGraph: {
-    title: "Better Auth Kit Github",
+    title: "Better-Auth-Kit Github",
     description: "The Better-Auth-Kit GitHub repository.",
     url: "https://github.com/ping-maxwell/better-auth-kit",
     siteName: "Better Auth Kit",
@@ -14,13 +14,13 @@ export const metadata: Metadata = {
         url: "https://github.com/ping-maxwell/better-auth-kit/blob/main/assets/banner/1600x600.png?raw=true",
         width: 1600,
         height: 600,
-        alt: "Better Auth Kit",
+        alt: "Better-Auth-Kit",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Better Auth Kit Github",
+    title: "Better-Auth-Kit Github",
     description: "The Better-Auth-Kit GitHub repository.",
     site: "@ping_maxwell",
     images: [
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
         url: "https://github.com/ping-maxwell/better-auth-kit/blob/main/assets/banner/1600x600.png?raw=true",
         width: 1600,
         height: 600,
-        alt: "Better Auth Kit",
+        alt: "Better-Auth-Kit",
       },
     ],
   },
