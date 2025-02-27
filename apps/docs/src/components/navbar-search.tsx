5c6e0d0e864930259417d0588ae52cf171dcc6d0
ping-maxwell <maxwell.multinite@gmail.com>
2025-02-28 01:05:25 +1000

    fix(docs): navbar search UI styles

--- apps/docs/src/components/navbar-search.tsx
+++ apps/docs/src/components/navbar-search.tsx
@@ -15,7 +15,7 @@ export const NavbarSearch = () => {
       }}
     >
       <Search className="sm:size-4 text-muted-foreground" />
-      <p className="hidden lg:inline">Search documentation...</p>
+      <p className="hidden sm:inline">Search documentation...</p>
       <kbd className="pointer-events-none hidden h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
         <span className="text-xs">⌘</span>K
       </kbd>
