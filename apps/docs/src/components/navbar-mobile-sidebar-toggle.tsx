d9406a0a00e586168ed156b15768ae8def3cf8bd
ping-maxwell <maxwell.multinite@gmail.com>
2025-02-27 22:33:55 +1000

    fix(docs): mobile navbar sidebar toggle button should hide on /

--- apps/docs/src/components/navbar-mobile-sidebar-toggle.tsx
+++ apps/docs/src/components/navbar-mobile-sidebar-toggle.tsx
@@ -2,14 +2,19 @@
 
 import { Menu } from "lucide-react";
 import { useMobileSidebar } from "./mobile-sidebar-controller";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 export const NavbarMobileSidebarToggle = () => {
   const { isOpen, setIsOpen } = useMobileSidebar();
-
+  const pathname = usePathname();
   return (
     <button
       type="button"
-      className="flex sm:hidden items-center gap-2 text-muted-foreground hover:text-foreground transition-color duration-150 ease-in-out ml-5"
+      className={cn(
+        "items-center gap-2 text-muted-foreground hover:text-foreground transition-color duration-150 ease-in-out ml-5",
+        pathname.startsWith("/docs") ? "flex sm:hidden" : "hidden"
+      )}
       onClick={() => setIsOpen(!isOpen)}
     >
       <Menu />
