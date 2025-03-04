e2bf0553106c7110979ffcf259280a71aa579a43
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-04 19:34:15 +1000

    add(docs): animation to comprehensive components

--- apps/docs/src/components/comprehensive.tsx
+++ apps/docs/src/components/comprehensive.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { type ReactNode, useEffect, useState } from "react";
 import { useComprehensive } from "./comprehensive-provider";
+import { motion } from "framer-motion";
+import useMeasure from "react-use-measure";
 
 export const Comprehensive = ({ children }: { children: ReactNode }) => {
   const { onChange } = useComprehensive();
   const [isShowing, setIsShowing] = useState(false);
+  const [ref, bounds] = useMeasure();
 
   useEffect(() => {
     const unsub = onChange((levelOfDetail) => {
@@ -13,5 +16,15 @@ export const Comprehensive = ({ children }: { children: ReactNode }) => {
     return unsub;
   }, [onChange]);
 
-  return isShowing ? children : null;
+  return (
+    <motion.div
+      animate={
+        isShowing
+          ? { opacity: 1, height: bounds.height, scale: 1 }
+          : { opacity: 0, height: 0, scale: 0 }
+      }
+    >
+      <div ref={ref}>{children}</div>
+    </motion.div>
+  );
 };
