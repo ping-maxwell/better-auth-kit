a0c1c3142e38a1a4367c0c9a60293b126aa35372
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-12 01:55:32 +1000

    fix(docs): themes not working

--- apps/docs/src/components/navbar-theme.tsx
+++ apps/docs/src/components/navbar-theme.tsx
@@ -3,16 +3,15 @@ import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
 
 export const NavbarTheme = () => {
-  const { setTheme, resolvedTheme, theme } = useTheme();
+  const { setTheme, resolvedTheme } = useTheme();
 
   return (
     <div className="w-fit px-5 h-6 flex items-center justify-center border-l border-muted">
       <button
         type="button"
         onClick={() => {
-			console.log('clicked', resolvedTheme, theme);
-			setTheme(resolvedTheme === "dark" ? "light" : "dark")
-		}}
+          setTheme(resolvedTheme === "dark" ? "light" : "dark");
+        }}
         className="flex items-center gap-2 text-muted-foreground hover:text-foreground transition-color duration-150 ease-in-out cursor-pointer"
         suppressHydrationWarning={true}
       >
