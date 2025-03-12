2d238fb9acea6e2ce0e632a47231ce173896fcb5
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-12 15:49:42 +1000

    add(ui): eye button to toggle visibility of password field

--- packages/libraries/ui/registry/new-york/password-field/password-field.tsx
+++ packages/libraries/ui/registry/new-york/password-field/password-field.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   FormControl,
   FormField,
@@ -6,27 +7,48 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Eye, EyeClosed } from "lucide-react";
+import { useState } from "react";
 import type { UseFormReturn } from "react-hook-form";
 
-export function PasswordField({ form, isLoading }: { form: UseFormReturn<any>, isLoading: boolean }) {
+export function PasswordField({
+  form,
+  isLoading,
+}: {
+  form: UseFormReturn<any>;
+  isLoading: boolean;
+}) {
+  const [hide, setHide] = useState(true);
   return (
-    <FormField
-      control={form.control}
-      name="password"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>Password</FormLabel>
-          <FormControl>
-            <Input
-              type="password"
-              placeholder="Enter your password"
-              disabled={isLoading}
-              {...field}
-            />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
+    <div className="relative">
+      <FormField
+        control={form.control}
+        name="password"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Password</FormLabel>
+            <FormControl>
+              <Input
+                type={hide ? "text" : "password"}
+                placeholder="Enter your password"
+                disabled={isLoading}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <Button
+        className="right-2 absolute top-7 size-6 z-10"
+        size={"sm"}
+        variant={"ghost"}
+        role="button"
+        type="button"
+        onClick={() => setHide(!hide)}
+      >
+        {hide ? <Eye /> : <EyeClosed />}
+      </Button>
+    </div>
   );
 }
