4d40b0752094492b79b91292b2071b84eab3f391
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-13 17:16:20 +1000

    add(docs): metadata & WIP:fix theme flashs

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -5,10 +5,21 @@ import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/next";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@/components/theme-provider";
+import { baseUrl, createMetadata } from "@/lib/metadata";
 
 const inter = Inter({
   subsets: ["latin"],
+});
+
+export const metadata = createMetadata({
+  title: {
+    template: "%s | Better Auth UI",
+    default: "Better Auth UI",
+  },
+  description:
+    "UI components for the most comprehensive authentication library for TypeScript.",
+  metadataBase: baseUrl,
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
@@ -17,38 +28,32 @@ export default function Layout({ children }: { children: ReactNode }) {
       <head>
         <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
         <script
-          // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: To apply theme immediately without flash.
           dangerouslySetInnerHTML={{
             __html: `
                     try {
                       if (localStorage.theme === 'dark' || ((!('theme' in localStorage) || localStorage.theme === 'system') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
                         document.querySelector('meta[name="theme-color"]').setAttribute('content')
-                      }
-                    } catch (_) {}
+						}
+						
+						} catch (_) {}
+						console.log('dark')
                   `,
           }}
         />
       </head>
-      <body className="flex flex-col min-h-screen overflow-hidden">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
+      <body className="flex flex-col min-h-screen overflow-hidden bg-background">
+        <RootProvider
+          theme={{
+            attribute: "class",
+          }}
         >
-          <RootProvider
-            theme={{
-              enabled: true,
-              defaultTheme: "dark",
-            }}
-          >
-            <MobileSidebarController>
-              {children}
-              <Toaster />
-            </MobileSidebarController>
-            <Analytics />
-          </RootProvider>
-        </ThemeProvider>
+          <MobileSidebarController>
+            {children}
+            <Toaster />
+          </MobileSidebarController>
+          <Analytics />
+        </RootProvider>
       </body>
     </html>
   );
