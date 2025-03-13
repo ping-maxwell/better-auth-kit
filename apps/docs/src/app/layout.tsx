753a78d7bc49fedaebccf5da7b2014f7e42188b1
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-13 16:39:29 +1000

    WIP-fix: next-theme flashing

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -5,23 +5,36 @@ import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/next";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({
-	subsets: ["latin"],
+  subsets: ["latin"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en" className={inter.className} suppressHydrationWarning>
-			<body className="flex flex-col min-h-screen overflow-hidden">
-				<MobileSidebarController>
-					<RootProvider>
-						{children}
-						<Toaster />
-					</RootProvider>
-				</MobileSidebarController>
-				<Analytics />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <body className="flex flex-col min-h-screen overflow-hidden">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <RootProvider
+            theme={{
+              enabled: true,
+			  defaultTheme: "dark"
+            }}
+          >
+            <MobileSidebarController>
+              {children}
+              <Toaster />
+            </MobileSidebarController>
+            <Analytics />
+          </RootProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
