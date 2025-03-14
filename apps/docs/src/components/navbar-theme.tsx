939ad872c397e34013b9f270e9a398332537f8b9
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-14 21:30:39 +1000

    add(docs): tooltip on theme toggle in navbar

--- apps/docs/src/components/navbar-theme.tsx
+++ apps/docs/src/components/navbar-theme.tsx
@@ -1,23 +1,34 @@
 "use client";
 import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import { TooltipProvider } from "./ui/tooltip";
 
 export const NavbarTheme = () => {
-	const { setTheme, resolvedTheme } = useTheme();
+  const { setTheme, resolvedTheme } = useTheme();
 
-	return (
-		<div className="w-fit px-5 h-6 flex items-center justify-center border-l border-muted">
-			<button
-				type="button"
-				onClick={() => {
-					setTheme(resolvedTheme === "dark" ? "light" : "dark");
-				}}
-				className="flex items-center gap-2 text-muted-foreground hover:text-foreground transition-color duration-150 ease-in-out cursor-pointer"
-				suppressHydrationWarning={true}
-			>
-				<Moon className="hidden dark:block" />
-				<Sun className="block dark:hidden" />
-			</button>
-		</div>
-	);
+  return (
+    <div className="w-fit px-5 h-6 flex items-center justify-center border-l border-muted">
+      <TooltipProvider delayDuration={100}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <button
+              type="button"
+              onClick={() => {
+                setTheme(resolvedTheme === "dark" ? "light" : "dark");
+              }}
+              className="flex items-center gap-2 text-muted-foreground hover:text-foreground transition-color duration-150 ease-in-out cursor-pointer"
+              suppressHydrationWarning={true}
+            >
+              <Moon className="hidden dark:block" />
+              <Sun className="block dark:hidden" />
+            </button>
+          </TooltipTrigger>
+          <TooltipContent className="mt-2">
+            <p>Toggle theme 🌙</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
 };
