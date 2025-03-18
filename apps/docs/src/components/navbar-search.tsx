39eee0b5090b8c212e5dba71804675f2c8efb891
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-19 03:05:16 +1000

    update(docs): navbar

--- apps/docs/src/components/navbar-search.tsx
+++ apps/docs/src/components/navbar-search.tsx
@@ -10,45 +10,36 @@ import { TooltipProvider } from "./ui/tooltip";
 import Link from "next/link";
 
 export const NavbarSearch = () => {
-	const { setOpenSearch } = useSearchContext();
-	const pathname = usePathname();
+  const { setOpenSearch } = useSearchContext();
+  const pathname = usePathname();
 
-	if (pathname.startsWith("/docs"))
-		return (
-			<div className="sm:mx-2  h-6 flex justify-center items-center sm:pl-2">
-				<Button
-					variant={"outline"}
-					className="relative flex gap-3 border-none! font-normal text-muted-foreground w-fit cursor-text shadow-none bg-transparent!"
-					onClick={() => {
-						setOpenSearch(true);
-					}}
-				>
-					<Search className="sm:size-4 text-muted-foreground" />
-					<p className="hidden sm:inline">Search documentation...</p>
-					<kbd className="pointer-events-none hidden h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-						<span className="text-xs">⌘</span>K
-					</kbd>
-				</Button>
-			</div>
-		);
+  if (pathname.startsWith("/docs"))
+    return (
+      <div className="sm:mx-2  h-6 flex justify-center items-center pl-2">
+        <Button
+          variant={"outline"}
+          className="relative flex gap-3 border-none! font-normal text-muted-foreground w-fit cursor-text shadow-none bg-transparent!"
+          onClick={() => {
+            setOpenSearch(true);
+          }}
+        >
+          <Search className="sm:size-4 text-muted-foreground" />
+          <p className="hidden sm:inline">Search documentation...</p>
+          <kbd className="pointer-events-none hidden h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+            <span className="text-xs">⌘</span>K
+          </kbd>
+        </Button>
+      </div>
+    );
 
-	return (
-		<div className="w-fit px-5 h-6 items-center justify-center">
-			<TooltipProvider delayDuration={100}>
-				<Tooltip>
-					<TooltipTrigger asChild>
-						<Link
-							href="/docs"
-							className="flex items-center gap-2 text-muted-foreground hover:text-foreground transition-color duration-150 ease-in-out"
-						>
-							<BookMarked />
-						</Link>
-					</TooltipTrigger>
-					<TooltipContent className="mt-2">
-						<p>Documentation 📚</p>
-					</TooltipContent>
-				</Tooltip>
-			</TooltipProvider>
-		</div>
-	);
+  return (
+    <div className="w-fit px-5 h-6 items-center justify-center">
+      <Link
+        href="/docs"
+        className="flex items-center gap-2 text-muted-foreground hover:text-foreground transition-color duration-150 ease-in-out"
+      >
+        Docs
+      </Link>
+    </div>
+  );
 };
--- apps/docs/src/components/navbar.tsx
+++ apps/docs/src/components/navbar.tsx
@@ -4,35 +4,29 @@ import Image from "next/image";
 import { NavbarSearch } from "./navbar-search";
 import { NavbarGithub } from "./navbar-github";
 import { NavbarMobileSidebarToggle } from "./navbar-mobile-sidebar-toggle";
-import { NavbarComponents } from "./navbar-components";
-import { NavbarTheme } from "./navbar-theme";
-import { NavbarRoadmap } from "./navbar-roadmap";
-import { NavbarCommunity } from "./navbar-community";
+import { NavbarDropdown } from "./navbar-dropdown";
 
 export const Navbar = () => {
-	return (
-		<header
-			className="fixed left-1/2 top-(--fd-banner-height) z-40 box-content w-full -translate-x-1/2 transition-colors border-b shadow-sm bg-fd-background/80 backdrop-blur-lg py-2 flex items-center justify-between"
-			aria-label="Main"
-			dir="ltr"
-		>
-			<NavbarMobileSidebarToggle />
-			<div className="h-12 w-full px-5">
-				<a
-					href="/"
-					className="flex justify-start items-center gap-2 h-full w-full text-nowrap"
-				>
-					<Image src={logo} alt="logo" width={32} height={32} />
-					Better-Auth-Kit
-				</a>
-			</div>
-			<div className="w-full"></div>
-			<NavbarSearch />
-			<NavbarCommunity />
-			<NavbarComponents />
-			<NavbarRoadmap />
-			<NavbarGithub />
-			<NavbarTheme />
-		</header>
-	);
+  return (
+    <header
+      className="fixed left-1/2 top-(--fd-banner-height) z-40 box-content w-full -translate-x-1/2 transition-colors border-b shadow-sm bg-fd-background/80 backdrop-blur-lg py-2 flex items-center justify-between"
+      aria-label="Main"
+      dir="ltr"
+    >
+      <NavbarMobileSidebarToggle />
+      <div className="h-12 px-5 mr-5">
+        <a
+          href="/"
+          className="flex justify-start items-center gap-2 h-full w-full text-nowrap"
+        >
+          <Image src={logo} alt="logo" width={32} height={32} />
+          Better-Auth-Kit
+        </a>
+      </div>
+      <div className="w-full"></div>
+      <NavbarSearch />
+      <NavbarGithub />
+      <NavbarDropdown />
+    </header>
+  );
 };
