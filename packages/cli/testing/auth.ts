c3d2a38f2772ee8a790a6fa42d08e3d6e2f22cdc
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-20 00:31:31 +1000

    update(docs): seeding for organizations

--- packages/cli/testing/auth.ts
+++ packages/cli/testing/auth.ts
@@ -28,7 +28,11 @@ export const auth = betterAuth({
 				},
 			},
 		},
-		organization(),
+		organization({
+			teams: {
+				enabled: true,
+			},
+		}),
 		admin(),
 		apiKey(),
 	],
--- packages/cli/testing/seed.ts
+++ packages/cli/testing/seed.ts
@@ -1,46 +1,9 @@
 import {
 	Seed,
-	$,
-	table,
-	type SeedConfig,
 	users,
 	organizations,
+	type SeedConfig,
 } from "@better-auth-kit/seed";
-import type { Session, User } from "./auth";
-import type { Account } from "better-auth/types";
-
-// export const seed = Seed({
-// 	user: table<User>({
-// 		id: $.uuid(),
-// 		name: $.first_and_lastname(),
-// 		email: $.email({ unique: true }),
-// 		emailVerified: $.randomBoolean(),
-// 		createdAt: $.randomDate(),
-// 		updatedAt: $.randomDate(),
-// 		banned: $.randomBoolean(),
-// 		banReason: $.custom(() => ""),
-// 		banExpires: $.randomDate(),
-// 		role: $.randomChoice(
-// 			$.custom(() => "admin"),
-// 			$.custom(() => "user"),
-// 		),
-// 	}),
-// 	session: table<Session>(
-// 		{
-// 			id: $.uuid(),
-// 			userId: $.foreignKey({ model: "user", field: "id", unique: true }),
-// 			createdAt: $.randomDate(),
-// 			updatedAt: $.randomDate(),
-// 			expiresAt: $.randomDate(),
-// 			token: $.uuid(),
-// 			ipAddress: $.custom(() => "127.0.0.1"),
-// 			userAgent: $.custom(() => "my-user-agent"),
-// 		},
-// 		{
-// 			count: 100,
-// 		},
-// 	),
-// });
 
 export const config: SeedConfig = {
 	deleteRowsBeforeSeeding: {
@@ -51,43 +14,19 @@ export const config: SeedConfig = {
 			"member",
 			"invitation",
 			"team",
-			"organization",
 			"user",
+			"organization",
 		],
 	},
 };
 
-// export const seed = Seed({
-// 	...users<{
-// 		user: User;
-// 		account: Account;
-// 		session: Session;
-// 	}>(
-// 		{
-// 			user: {
-// 				banned: $.randomBoolean(),
-// 				banReason: $.custom(() => ""),
-// 				banExpires: $.randomDate(),
-// 				role: $.randomChoice(
-// 					$.custom(() => "admin"),
-// 					$.custom(() => "user"),
-// 				),
-// 			},
-// 		},
-// 		{
-// 			count: 10000,
-// 		},
-// 	),
-// });
-
 export const seed = Seed({
-	...users({}, { count: 10000 }),
+	...users({}),
 	...organizations(
 		{},
 		{
-			// createInvitations: true,
-			// createTeams: true,
-			count: 10000,
+			createTeams: true,
+			createInvitations: true,
 		},
 	),
 });
