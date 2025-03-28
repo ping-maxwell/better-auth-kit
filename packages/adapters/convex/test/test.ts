017678826a60b3e2e7195740ed17d5fc2c2723be
ping-maxwell <maxwell.multinite@gmail.com>
2025-03-29 08:01:44 +1000

    update(convex): schema generation tests

--- packages/adapters/convex/test/test.ts
+++ packages/adapters/convex/test/test.ts
@@ -1,9 +1,12 @@
 import { betterAuth } from "better-auth";
 import { convexAdapter } from "./../src";
 import { fullApi } from "../convex/_generated/api";
+import { ConvexHttpClient } from "convex/browser";
+
+const client = new ConvexHttpClient(process.env.CONVEX_URL!);
 
 export const auth = betterAuth({
-	// database: convexAdapter(),
+	database: convexAdapter(client, { enable_debug_logs: true }),
 	plugins: [],
 	//... other options
 });
