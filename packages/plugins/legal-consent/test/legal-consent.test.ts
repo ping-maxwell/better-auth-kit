36c19d87e97760733794c9614f6a352fe8151383
ping-maxwell <maxwell.multinite@gmail.com>
2025-05-12 08:05:12 +1000

    tests: Added more legal-consent plugin tests

--- packages/plugins/legal-consent/test/legal-consent.test.ts
+++ packages/plugins/legal-consent/test/legal-consent.test.ts
@@ -2,7 +2,7 @@ import { getTestInstance, tryCatch } from "@better-auth-kit/tests";
 import { beforeAll, describe, expect, it } from "vitest";
 import { auth } from "./auth";
 
-const { resetDatabase } = await getTestInstance(auth);
+const { resetDatabase, client } = await getTestInstance(auth);
 
 describe("Legal Consent Plugin", () => {
 	beforeAll(async () => {
@@ -36,6 +36,37 @@ describe("Legal Consent Plugin", () => {
 				},
 			}),
 		);
+		expect(data).toBeDefined();
+		expect(error).toBeNull();
+	});
+});
+
+describe("Legal Consent Plugin - Auth Client", () => {
+	beforeAll(async () => {
+		await resetDatabase();
+	});
+
+	it("Should fail if TOS is not accepted", async () => {
+		const { data, error } = await client.signUp.email({
+			email: "test@test.com",
+			password: crypto.randomUUID(),
+			name: "test user",
+			//@ts-ignore
+			tosAccepted: false,
+		});
+		expect(data).toBeNull();
+		expect(error).toBeDefined();
+		expect(error?.code).toEqual("TOS_MUST_BE_ACCEPTED");
+	});
+
+	it("Should succeed if TOS is accepted", async () => {
+		const { data, error } = await client.signUp.email({
+			email: "test@test.com",
+			password: crypto.randomUUID(),
+			name: "test user",
+			//@ts-ignore
+			tosAccepted: true,
+		});
 		expect(data).toBeDefined();
 		expect(error).toBeNull();
 	});
