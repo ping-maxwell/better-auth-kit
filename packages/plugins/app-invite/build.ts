5f0c1be93e9500c0e9ca8e4ea86a625eea0e2521
ping-maxwell <maxwell.multinite@gmail.com>
2025-06-02 03:38:01 +1000

    fix: app-invite not having client bundle

--- packages/plugins/app-invite/build.ts
+++ packages/plugins/app-invite/build.ts
@@ -2,5 +2,6 @@ import { build, type Config } from "@better-auth-kit/internal-build";
 
 export const config: Config = {
 	enableDts: true,
+	entrypoints: ['./src/index.ts', './src/client.ts'],
 };
 build(config);
